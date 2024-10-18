fdbd39fb9b790bf3c9dc8fe74f3c023ba0cfe660
Fomentr <167105040+0x416E50@users.noreply.github.com>
2024-10-18 11:26:11 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,6 @@
-// config/db.js
 const mongoose = require('mongoose');
-// require('dotenv').config(); // Load environment variables from .env file
 
+// Load environment variables from .env file
 const MONGO_URI="mongodb://127.0.0.1:27017/ruleengine"
 
 const connectDB = async () => {
