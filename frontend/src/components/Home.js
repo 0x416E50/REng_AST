fca68ab80f13e0f65e228ae4c76c0fd0df8fa8be
Fomentr <167105040+0x416E50@users.noreply.github.com>
2024-10-19 12:26:42 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100" style={{ paddingTop: '50px' }}> {/* Adjust padding-top to move heading up */}
-        <h1 className="text-4xl font-bold mb-4">Rule Engine Using AST</h1> {/* Reduced margin-bottom for closer positioning */}
+        <h1 className="text-4xl font-bold mb-4">Rule Engine Using Abstract Syntax Tree</h1> {/* Reduced margin-bottom for closer positioning */}
         <div className="flex flex-col gap-1"> {/* Add gap between buttons */}
           <Link to="/create-rule">
             <button className="w-48 px-4 py-2 bg-gray-400 text-white rounded-md shadow-md hover:bg-gray-500 transition duration-300">
